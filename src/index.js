e31f6ec4c9324208d1d806a533e73517aa34d527
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2019-03-19 18:45:47 +0300

    feat: add brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var array = str.split('');
+  
+
+  for (var i = 0; i<array.length; i++ ){
+    for(var j = 0; j<bracketsConfig.length; j++){
+      if(array[i] == bracketsConfig[j][1]){
+        if(array[i-1] == bracketsConfig[j][0]){ 
+          delete array[i];
+          delete array[i-1];
+          i = 0;
+          var array = array.filter(Boolean);
+        }
+        else{
+          continue;
+        }
+      }
+      else{
+        continue;
+      }
+    }
+  }
+  if(array.length == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
